991e9eb007474594afe84551d0fbbfeec981101e
Paul Chin Jr <pchinjr@gmail.com>
2020-10-19 10:42:49 -0400

    verify token

--- src/http/get-verify-000token/index.js
+++ src/http/get-verify-000token/index.js
@@ -1,16 +1,21 @@
 let arc = require('@architect/functions')
 let layout = require('@architect/views/layout')
+let data = require('@begin/data')
 
 exports.handler = arc.http.async(index)
 
 async function index(req) {
   let token = req.params.token
 
-  return {
-    html: layout({
-      account: req.session.account,
-      body: `verifying email ... with token: ${token}`
-    })
+  //compare token
+  let result = await data.get({table: 'tokens', key: token})
+  if (result.key === token) {
+    return {
+      html: layout({
+        account: req.session.account,
+        body: `verifying email ... with token: ${token}`
+      })
+    }
   }
 
 
